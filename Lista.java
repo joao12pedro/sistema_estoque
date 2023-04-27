290ebf348f41771ebe841c71bf428cfb893da80c
joao12pedro <114536833+joao12pedro@users.noreply.github.com>
2023-04-27 19:53:56 -0300

    Update Lista.java

--- Lista.java
+++ Lista.java
@@ -13,9 +13,35 @@ public class Lista {
 	public void adicionarProduto(Produto produto) {
 		produtos.add(produto);
 	}
-	public List<Produto> retornaProdutos(){
+	public void retornaProdutos(){
+		for (int i = 0; i < produtos.size(); i++) {
+			System.out.println(i+1 + "° produto:" + produtos.get(i).getNome());
+		}
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public List<Produto> getProdutos() {
 		return produtos;
+	}
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	public void exclui(Produto produto) {
+		produtos.remove(produto);
+		System.out.println("Produto removido:" + produto.getNome());
+	}
+	public void ordenaNome() {
+		Collections.sort(produtos);
+		System.out.println("Ordenados por nome:");
+		for (int i = 0; i < produtos.size(); i++) {
+			System.out.println(i+1 + "° produto:" + produtos.get(i).getNome());
+		}
 	}
 	
 
 }
+
